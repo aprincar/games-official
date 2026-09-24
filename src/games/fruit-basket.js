65025d3f301ba39740b569efab9791d14fca004d
Kiver Teixeira <kivervinicius@hotmail.com>
2026-09-24 17:56:27 -0300

    fix: drag fruit containers instead of nested hit zones

--- src/games/fruit-basket.js
+++ src/games/fruit-basket.js
@@ -43,12 +43,13 @@
         window.AprincarVectorArt.drawFruit(fruitGfx, fruitType, 0, 0, 58);
         const hitZone = this.add.circle(0, 0, 44, 0xffffff, 0.001);
         fruitContainer.add([fruitGfx, hitZone]);
+        fruitContainer.setSize(88, 88);
         this.roundGroup.add(fruitContainer);
 
         this.target(`fruit-${i + 1}`, x, y, 88, 88, 'drag-source');
         this.target(`fruit-${i + 1}`, x, y, 88, 88, 'toggle');
 
-        window.AprincarInputGestures.attachTapOrDrag(this, hitZone, {
+        window.AprincarInputGestures.attachTapOrDrag(this, fruitContainer, {
           threshold: 10,
           onDrag: (pointer) => {
             fruitContainer.x = pointer.x;
